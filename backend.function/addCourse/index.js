df29c75c2b3b807d282966b04811a2b07a351ec7
Amirtha <amirtha.sr.2024.cse@rajalakshmi.edu.in>
2026-04-23 19:41:04 +0530

    fix: use correct DB config in addCourse

--- backend.function/addCourse/index.js
+++ backend.function/addCourse/index.js
@@ -1,11 +1,11 @@
 const sql = require('mssql');
 
 const config = {
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
+  user: 'edusphereadmin',
   password: process.env.DB_PASSWORD,
-  options: { encrypt: true, trustServerCertificate: false }
+  server: 'edusphere-db-server.database.windows.net',
+  database: 'edusphere-db',
+  options: { encrypt: true }
 };
 
 module.exports = async function (context, req) {
@@ -33,14 +33,14 @@ module.exports = async function (context, req) {
   }
 
   try {
-    const pool = await sql.connect(config);
+    await sql.connect(config);
 
-    await pool.request()
-      .input('title', sql.NVarChar, title)
-      .input('description', sql.NVarChar, description)
-      .input('video_url', sql.NVarChar, video_url || null)
-      .query(`INSERT INTO Courses (title, description, video_url)
-              VALUES (@title, @description, @video_url)`);
+    await sql.query`
+      INSERT INTO Courses (title, description, video_url)
+      VALUES (${title}, ${description}, ${video_url || null})
+    `;
+
+    await sql.close();
 
     context.res = {
       status: 201,
@@ -48,6 +48,7 @@ module.exports = async function (context, req) {
       body: { message: 'Course added successfully' }
     };
   } catch (err) {
+    await sql.close().catch(() => {});
     context.res = {
       status: 500,
       headers: { 'Access-Control-Allow-Origin': '*' },
